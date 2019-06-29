6c544736e6fe12884206e3f330ce4ba69610475c
Olena Rostotskyy <olenazachkow@gmail.com>
2019-06-29 09:18:45 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '385243245678433',
+        clientSecret: 'c3e56c2c19972383cde24b421ba25a12'
+    }
 }
